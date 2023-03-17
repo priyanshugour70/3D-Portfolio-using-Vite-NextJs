483434930a203b7eff5fca062531e6066a63a56e
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-17 22:32:19 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-const App = () => {
-  return (
-      <div>
-        3D Portfolio
-      </div>
-  )
-}
+import { BrowserRouter } from "react-router-dom";
+import {
+    About,
+    Contact,
+    Experience,
+    Feedbacks,
+    Hero,
+    Navbar,
+    Tech,
+    Works,
+    StarsCanvas,
+} from "./components";
 
-export default App
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="relative z-0 bg-primary">
+                <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                    <Navbar />
+                    <Hero />
+                </div>
+                <About />
+                <Experience />
+                <Tech />
+                <Works />
+                <Feedbacks />
+                <div className="relative z-0">
+                    <Contact />
+                    <StarsCanvas />
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+};
+
+export default App;
